f59c6838a85e9d1b6e2e2f9ad79d709df0fd8602
foysal41 <mdfoysal.mf33@gmail.com>
2026-06-05 11:39:33 +0600

    here installed tools and connect with mongodb then worked with job post api

---
+++ index.js
@@ -0,0 +1,63 @@
+
+const express = require("express");
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+const uri = "mongodb+srv://nesthire:nesthire@cluster0.8xvidah.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+   const database = client.db("nextloop_db");
+    const jobCollection = database.collection("jobs");
+
+app.post("/api/jobs", async(req, res)=>{
+    const job = req.body;
+    const result = await jobCollection.insertOne(job)
+    res.send(result)
+})
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
